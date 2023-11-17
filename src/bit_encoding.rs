bcbb771b89280dd3a775344a87566c89a9cf2400
Christian Lewe <ch_lewe@agdsn.me>
2023-11-17 13:49:06 +0100

    Cargo: Update simplicity

--- src/bit_encoding.rs
+++ src/bit_encoding.rs
@@ -22,7 +22,7 @@ impl<W: io::Write> Encoder<W> {
     }
 
     pub fn program_preamble(&mut self, len: usize) -> io::Result<()> {
-        encode::encode_natural(len, &mut self.bits)
+        encode::encode_natural(len, &mut self.bits).map(|_| ())
     }
 
     pub fn unit(&mut self) -> io::Result<()> {
@@ -36,25 +36,25 @@ impl<W: io::Write> Encoder<W> {
     pub fn comp(&mut self, left_offset: usize, right_offset: usize) -> io::Result<()> {
         self.bits.write_bits_be(0x00000, 5)?;
         encode::encode_natural(left_offset, &mut self.bits)?;
-        encode::encode_natural(right_offset, &mut self.bits)
+        encode::encode_natural(right_offset, &mut self.bits).map(|_| ())
     }
 
     pub fn case(&mut self, left_offset: usize, right_offset: usize) -> io::Result<()> {
         self.bits.write_bits_be(0b00001, 5)?;
         encode::encode_natural(left_offset, &mut self.bits)?;
-        encode::encode_natural(right_offset, &mut self.bits)
+        encode::encode_natural(right_offset, &mut self.bits).map(|_| ())
     }
 
     pub fn hidden(&mut self, payload: &[u8]) -> io::Result<()> {
         self.bits.write_bits_be(0b0110, 4)?;
         // 2023-11-16: We can use this method because it does not check the length of `payload`
-        encode::encode_hash(payload, &mut self.bits)
+        encode::encode_hash(payload, &mut self.bits).map(|_| ())
     }
 
     pub fn fail(&mut self, entropy: &[u8]) -> io::Result<()> {
         self.bits.write_bits_be(0b01010, 5)?;
         // 2023-11-16: We can use this method because it does not check the length of `entropy`
-        encode::encode_hash(entropy, &mut self.bits)
+        encode::encode_hash(entropy, &mut self.bits).map(|_| ())
     }
 
     pub fn stop(&mut self) -> io::Result<()> {
@@ -71,7 +71,7 @@ impl<W: io::Write> Encoder<W> {
         self.bits.write_bit(true)?;
         self.bits.write_bit(false)?;
         encode::encode_natural(depth, &mut self.bits)?;
-        encode::encode_value(value, &mut self.bits)
+        encode::encode_value(value, &mut self.bits).map(|_| ())
     }
 
     pub fn witness_preamble(&mut self, len: Option<usize>) -> io::Result<()> {
@@ -79,7 +79,7 @@ impl<W: io::Write> Encoder<W> {
             None => self.bits.write_bit(false),
             Some(len) => {
                 self.bits.write_bit(true)?;
-                encode::encode_natural(len, &mut self.bits)
+                encode::encode_natural(len, &mut self.bits).map(|_| ())
             }
         }
     }
