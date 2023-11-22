019e60b9e3dad43bf709fa11b572d2f943d17ad3
Christian Lewe <ch_lewe@agdsn.me>
2023-11-22 22:55:12 +0100

    Test: Keep track of builder state via generics

--- src/test.rs
+++ src/test.rs
@@ -9,91 +9,133 @@ use simplicity::RedeemNode;
 use crate::json::{Flag, Parameters, ScriptError, Serde, TestCase};
 use crate::util;
 
-#[derive(Debug)]
-pub struct TestBuilder(Result<BuilderInner, String>);
+pub trait MaybeBytes {}
+pub struct NoBytes;
+pub struct Bytes(Vec<u8>);
+impl MaybeBytes for NoBytes {}
+impl MaybeBytes for Bytes {}
 
-impl TestBuilder {
+pub trait MaybeCmr {}
+pub struct NoCmr;
+pub struct Cmr(Vec<u8>);
+impl MaybeCmr for NoCmr {}
+impl MaybeCmr for Cmr {}
+
+pub trait MaybeError {}
+pub struct NoError;
+pub struct Error(ScriptError);
+impl MaybeError for NoError {}
+impl MaybeError for Error {}
+
+#[derive(Debug)]
+pub struct TestBuilder<B: MaybeBytes, C: MaybeCmr, E: MaybeError>(
+    Result<BuilderInner<B, C, E>, String>,
+);
+
+impl TestBuilder<NoBytes, NoCmr, NoError> {
     pub fn comment(comment: &'static str) -> Self {
         Self(Ok(BuilderInner::comment(comment)))
     }
+}
 
-    pub fn raw_program(self, bytes: Vec<u8>) -> Self {
-        Self(self.0.map(|inner| inner.raw_program(bytes)))
+impl<B: MaybeBytes, C: MaybeCmr, E: MaybeError> TestBuilder<B, C, E> {
+    pub fn raw_program(self, bytes: Vec<u8>) -> TestBuilder<Bytes, C, E> {
+        TestBuilder(self.0.map(|inner| inner.raw_program(bytes)))
     }
 
-    pub fn raw_cmr<A: AsRef<[u8]>>(self, cmr: A) -> Self {
-        Self(self.0.map(|inner| inner.raw_cmr(cmr)))
+    pub fn raw_cmr<A: AsRef<[u8]>>(self, cmr: A) -> TestBuilder<B, Cmr, E> {
+        TestBuilder(self.0.map(|inner| inner.raw_cmr(cmr)))
     }
 
-    pub fn program(self, program: &RedeemNode<Elements>) -> Self {
-        Self(self.0.map(|inner| inner.program(program)))
+    pub fn program(self, program: &RedeemNode<Elements>) -> TestBuilder<Bytes, Cmr, E> {
+        TestBuilder(self.0.map(|inner| inner.program(program)))
     }
 
     pub fn human_encoding(
         self,
         s: &str,
         witness: &HashMap<Arc<str>, Arc<simplicity::Value>>,
-    ) -> Self {
-        Self(self.0.and_then(|inner| inner.human_encoding(s, witness)))
+    ) -> TestBuilder<Bytes, Cmr, E> {
+        TestBuilder(self.0.and_then(|inner| inner.human_encoding(s, witness)))
     }
 
     pub fn extra_script_input(self, script_input: Vec<u8>) -> Self {
         Self(self.0.map(|inner| inner.extra_script_input(script_input)))
     }
 
-    pub fn expected_result(self, error: ScriptError) -> Self {
-        Self(self.0.map(|inner| inner.expected_result(error)))
+    pub fn expected_result(self, error: ScriptError) -> TestBuilder<B, C, Error> {
+        TestBuilder(self.0.map(|inner| inner.expected_result(error)))
     }
+}
 
+impl TestBuilder<Bytes, Cmr, Error> {
     pub fn finished(self) -> Result<TestCase, String> {
         self.0.map(|inner| inner.finished())
     }
 }
 
 #[derive(Debug)]
-struct BuilderInner {
+struct BuilderInner<B: MaybeBytes, C: MaybeCmr, E: MaybeError> {
     comment: &'static str,
-    program_bytes: Option<Vec<u8>>,
-    cmr: Option<Vec<u8>>,
+    program_bytes: B,
+    cmr: C,
     extra_script_inputs: Vec<Vec<u8>>,
     cost: Option<simplicity::Cost>,
-    error: Option<ScriptError>,
+    error: E,
 }
 
-impl BuilderInner {
+impl BuilderInner<NoBytes, NoCmr, NoError> {
     pub fn comment(comment: &'static str) -> Self {
         Self {
             comment,
-            program_bytes: None,
-            cmr: None,
+            program_bytes: NoBytes,
+            cmr: NoCmr,
             extra_script_inputs: vec![],
             cost: None,
-            error: None,
+            error: NoError,
+        }
+    }
+}
+
+impl<B: MaybeBytes, C: MaybeCmr, E: MaybeError> BuilderInner<B, C, E> {
+    pub fn raw_program(self, bytes: Vec<u8>) -> BuilderInner<Bytes, C, E> {
+        BuilderInner {
+            comment: self.comment,
+            program_bytes: Bytes(bytes),
+            cmr: self.cmr,
+            extra_script_inputs: self.extra_script_inputs,
+            cost: self.cost,
+            error: self.error,
         }
     }
 
-    pub fn raw_program(mut self, bytes: Vec<u8>) -> Self {
-        self.program_bytes = Some(bytes);
-        self
+    pub fn raw_cmr<A: AsRef<[u8]>>(self, cmr: A) -> BuilderInner<B, Cmr, E> {
+        BuilderInner {
+            comment: self.comment,
+            program_bytes: self.program_bytes,
+            cmr: Cmr(cmr.as_ref().to_vec()),
+            extra_script_inputs: self.extra_script_inputs,
+            cost: self.cost,
+            error: self.error,
+        }
     }
 
-    pub fn raw_cmr<A: AsRef<[u8]>>(mut self, cmr: A) -> Self {
-        self.cmr = Some(cmr.as_ref().to_vec());
-        self
-    }
-
-    pub fn program(mut self, program: &RedeemNode<Elements>) -> Self {
-        self.program_bytes = Some(program.encode_to_vec());
-        self.cmr = Some(program.cmr().to_byte_array().to_vec());
-        self.cost = Some(program.bounds().cost);
-        self
+    pub fn program(self, program: &RedeemNode<Elements>) -> BuilderInner<Bytes, Cmr, E> {
+        BuilderInner {
+            comment: self.comment,
+            program_bytes: Bytes(program.encode_to_vec()),
+            cmr: Cmr(program.cmr().to_byte_array().to_vec()),
+            extra_script_inputs: self.extra_script_inputs,
+            cost: Some(program.bounds().cost),
+            error: self.error,
+        }
     }
 
     pub fn human_encoding(
         self,
         s: &str,
         witness: &HashMap<Arc<str>, Arc<simplicity::Value>>,
-    ) -> Result<Self, String> {
+    ) -> Result<BuilderInner<Bytes, Cmr, E>, String> {
         let program = util::program_from_string::<Elements>(s, witness)?;
         Ok(self.program(&program))
     }
@@ -103,30 +145,37 @@ impl BuilderInner {
         self
     }
 
-    pub fn expected_result(mut self, error: ScriptError) -> Self {
-        self.error = Some(error);
-        self
+    pub fn expected_result(self, error: ScriptError) -> BuilderInner<B, C, Error> {
+        BuilderInner {
+            comment: self.comment,
+            program_bytes: self.program_bytes,
+            cmr: self.cmr,
+            extra_script_inputs: self.extra_script_inputs,
+            cost: self.cost,
+            error: Error(error),
+        }
     }
+}
 
+impl BuilderInner<Bytes, Cmr, Error> {
     pub fn finished(self) -> TestCase {
-        let program_bytes = self.program_bytes.expect("required");
-        let commit = self.cmr.expect("required");
-        let error = match self.error.expect("required") {
+        let program_bytes = self.program_bytes.0;
+        let cmr = self.cmr.0;
+        let error = match self.error.0 {
             ScriptError::Ok => None,
             error => Some(error),
         };
 
-        let spend_info = util::get_spend_info(commit.clone(), simplicity::leaf_version());
+        let spend_info = util::get_spend_info(cmr.clone(), simplicity::leaf_version());
         let control_block =
-            util::get_control_block(commit.clone(), simplicity::leaf_version(), &spend_info)
-                .unwrap();
+            util::get_control_block(cmr.clone(), simplicity::leaf_version(), &spend_info).unwrap();
 
         let funding_tx = get_funding_tx(&spend_info);
         let spending_tx = get_spending_tx(&funding_tx);
 
         let mut script_inputs = vec![program_bytes];
         script_inputs.extend(self.extra_script_inputs);
-        let script = util::to_script(commit);
+        let script = util::to_script(cmr);
         let mut witness = util::get_witness_stack(script_inputs, script, control_block);
 
         if let Some(cost) = self.cost {
