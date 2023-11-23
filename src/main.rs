baaf0dc909f34dfc5cabd3fcdf4fa5ecaadc9833
Christian Lewe <ch_lewe@agdsn.me>
2023-11-23 15:54:43 +0100

    Main: Update error message

--- src/main.rs
+++ src/main.rs
@@ -1080,7 +1080,7 @@ fn main() {
     /*
      * Export test cases to JSON
      */
-    let s = serde_json::to_string_pretty(&test_cases).expect("serialize");
+    let s = serde_json::to_string_pretty(&test_cases).expect("Unable to create JSON");
     let mut file = File::create("script_assets_test.json").expect("Unable to create file");
     file.write_all(s.as_bytes()).expect("Unable to write data");
 }
